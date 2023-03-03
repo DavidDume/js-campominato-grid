586599e6d64375578f7090b89eeeae32d5fb9fa2
David Dume <dumedavid11@gmail.com>
2023-03-03 16:03:17 +0100

    aggiunta modalita di gioco

--- main.js
+++ main.js
@@ -1,17 +1,42 @@
 const gridDom = document.querySelector(".grid");
 
-for(let i = 1; i <= 100; i++) {
-    let box = document.createElement('div');
-    box.classList.add('box');
-    box.addEventListener('click', function() {
-        
-        //mostra il numero solo se box non è ancora attiva
-        if(!this.classList.contains('clicked')) {
-            console.log(i);
-        }
-        this.classList.toggle('clicked');
-        
-    })
-    gridDom.append(box);
+const selectDom = document.querySelector('#select');
 
+const playBtn = document.querySelector('#play');
+
+gameMode(parseInt(selectDom.value));
+
+playBtn.addEventListener('click', function() {
+    gameMode(parseInt(selectDom.value))
+})
+
+function gameMode(mode) {
+    // resetto il grid ogni volta che cambio modalita di gioco
+    gridDom.innerHTML = ""
+
+    let classText;
+
+    if(mode == 100) {
+        classText = "facile";
+    } else if(mode == 81) {
+        classText = "intermedio";
+    } else if(mode == 49) {
+        classText = "difficile";
+    }
+
+    for(let i = 1; i <= mode; i++) {
+        let box = document.createElement('div');
+        box.classList.add(classText);
+        box.addEventListener('click', function() {
+    
+            //mostra il numero solo se box non è ancora attiva
+            if(!this.classList.contains('clicked')) {
+                console.log(i);
+            }
+            this.classList.toggle('clicked');
+            
+        })
+        gridDom.append(box);
+    
+    }
 }
