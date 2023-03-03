5f40b53cf3e22d89f3c3f6ac255e488eb82dcc23
David Dume <dumedavid11@gmail.com>
2023-03-03 15:41:24 +0100

    codice per mostrare il numero della casella

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const gridDom = document.querySelector(".grid");
 
-for(let i = 0; i < 100; i++) {
+for(let i = 1; i <= 100; i++) {
     let box = document.createElement('div');
     box.classList.add('box');
+    box.addEventListener('click', function() {
+        
+        //mostra il numero solo se box non è ancora attiva
+        if(!this.classList.contains('clicked')) {
+            console.log(i);
+        }
+        this.classList.toggle('clicked');
+        
+    })
     gridDom.append(box);
+
 }
